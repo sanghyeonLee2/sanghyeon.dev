8be5088e2e8f1ad8398296a9ff65714f50ec23a3
SanghyeonLee <hyeon012366@gmail.com>
2025-06-05 21:24:57 +0900

    init: 초기 셋팅 및 framer-motion 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export const dynamic = 'force-static';
-
 import HomeTemplate from '@/components/templates/Home';
 
-export default async function Home() {
-  return <HomeTemplate/>;
+export default function Home() {
+  <HomeTemplate />;
 }
--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,7 +1,1 @@
-export const DEFAULTS = {
-  TITLE: '제목 없음',
-  EMPTY_STRING: '',
-  PLACEHOLDER_STRING: null as string | null,
-  IMG_URL:
-    'https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-};
+export const DEFAULTS = {};
