05823a61a03104e1ae58d97f294b4868b3f95573
SanghyeonLee <hyeon012366@gmail.com>
2025-06-08 18:56:07 +0900

    design: 직관성, 가독성 고려해 스타일 수정

--- src/components/section/Experience/ExperienceList.tsx
+++ src/components/section/Experience/ExperienceList.tsx
@@ -6,19 +6,24 @@ const ExperienceList = ({ experiences }: { experiences: ExperienceType[] }) => {
     <ul className="divide-y divide-gray-300 mb-8">
       {experiences.map((experience) => (
         <li key={experience.name}>
-          <article className="flex flex-col md:flex-row gap-6 p-content my-3">
-            <div className=" flex-col md:items-start md:w-60 flex-shrink-0">
-              <h3 className="text-2xl font-semibold text-center">{experience.name}</h3>
-              <p className="text-sm text-gray-400 text-center">{experience.period}</p>
+          <article className="flex flex-col md:flex-row gap-6 p-9 my-3">
+            <div className="flex-center">
+              <div className="flex-col md:items-start md:w-70 flex-shrink-0">
+                <h3 className="text-2xl font-semibold content-center text-center">
+                  {experience.name}
+                </h3>
+                <p className="text-sm text-gray-400 text-center">{experience.period}</p>
+              </div>
             </div>
-
-            <ul className="list-disc pl-5 flex-grow flex flex-col gap-2">
-              {experience.contents.map((content) => (
-                <li className="md:text-center" key={content}>
-                  <p>{content}</p>
-                </li>
-              ))}
-            </ul>
+            <div className="content-center">
+              <ul className="list-disc pl-5 flex-grow flex flex-col gap-2">
+                {experience.contents.map((content) => (
+                  <li key={content}>
+                    <p>{content}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </article>
         </li>
       ))}
