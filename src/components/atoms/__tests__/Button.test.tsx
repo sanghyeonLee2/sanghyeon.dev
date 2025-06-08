202130bfc39c51cc7cc26596432231ccfa8ad054
SanghyeonLee <hyeon012366@gmail.com>
2025-06-08 17:55:54 +0900

    remove: 불필요한 테스트 관련 파일 삭제

--- src/components/atoms/__tests__/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Button from '../button/Button';
-
-describe('Button', () => {
-  it('텍스트가 제대로 렌더링되는지 확인', () => {
-    render(<Button text="확인" />);
-    expect(screen.getByText('확인')).toBeInTheDocument();
-  });
-});
