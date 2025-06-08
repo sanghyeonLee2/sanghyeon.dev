6892cc860f68c838ad59fc6162f7421a3cca698f
SanghyeonLee <hyeon012366@gmail.com>
2025-06-08 18:00:49 +0900

    feat: ui 타입 추가

---
+++ src/types/ui.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export type DropdownOptions = {
+  value: 'light' | 'dark' | 'system';
+  label: string;
+  icon: React.ReactNode;
+};
+
+export interface LinkType extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  text?: string;
+  icon?: React.ReactNode;
+}
