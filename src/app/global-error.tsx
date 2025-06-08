8d3a11f39071932b53e33e26d410f0937b4b32fd
SanghyeonLee <hyeon012366@gmail.com>
2025-06-08 18:06:51 +0900

    remove: 불필요 파일 제거

--- src/app/global-error.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import GlobalErrorFallback from '@/components/templates/GlobalErrorFallback';
-
-export default function GlobalError({ error }: { error: Error }) {
-  return <GlobalErrorFallback error={error} />;
-}
