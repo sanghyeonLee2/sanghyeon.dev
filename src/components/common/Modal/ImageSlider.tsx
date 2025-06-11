b3993c1ae6e4e6790c0a1764335f026069a31e38
SanghyeonLee <hyeon012366@gmail.com>
2025-06-11 20:21:36 +0900

    feat: 이미지 슬라이더 컴포넌트 추가

---
+++ src/components/common/Modal/ImageSlider.tsx
@@ -0,0 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import ImageComponent from '@/components/common/Image/ImageComponent';
+import { StaticImageData } from 'next/image';
+
+const ImageSlider = ({ imgs }: { imgs: StaticImageData[] }) => {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      navigation
+      spaceBetween={20}
+      breakpoints={{
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+      }}
+      className="w-full h-70 mb-4"
+    >
+      {imgs.map((img) => (
+        <SwiperSlide key={img.src}>
+          <ImageComponent
+            src={img}
+            alt={`슬라이드 ${img.src}`}
+            className="shadow-[var(--shadow)] rounded-lg h-full"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ImageSlider;
