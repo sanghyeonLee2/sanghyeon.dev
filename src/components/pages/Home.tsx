6ef365505c6ea7fb229f93489e7a65c9ecf6cf0b
SanghyeonLee <hyeon012366@gmail.com>
2025-06-13 21:20:13 +0900

    refactor: 스타일 수정 및 상수 수정

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,22 +1,22 @@
 'use client';
 
-import About from '@/components/section/About';
-import Experience from '@/components/section/Experience/Experience';
+import AboutSection from '@/components/section/About/AboutSection';
+import Experience from '@/components/section/Experience/ExperienceSection';
 import Intro from '@/components/section/Intro';
 import Layout from '@/components/section/Layout';
-import Projects from '@/components/section/Projects';
-import Skills from '@/components/section/Skills';
+import ProjectsSection from '@/components/section/Projects/ProjectsSection';
+import SkillsSection from '@/components/section/Skills/SkillsSection';
 import { SectionId } from '@/constants/section';
 
 const sections: { id: SectionId; Component: React.FC }[] = [
-  { id: 'about', Component: About },
-  { id: 'projects', Component: Projects },
-  { id: 'skills', Component: Skills },
+  { id: 'about', Component: AboutSection },
+  { id: 'projects', Component: ProjectsSection },
+  { id: 'skills', Component: SkillsSection },
   { id: 'experience', Component: Experience },
 ];
 export default function Home() {
   return (
-    <main className="flex-grow layout-wrap-max-w w-full px-content mx-auto mb-20">
+    <main className="flex-1 layout-wrap-max-w w-full px-content mx-auto mb-20">
       <Intro />
       {sections.map(({ id, Component }) => (
         <Layout key={id} sectionId={id}>
