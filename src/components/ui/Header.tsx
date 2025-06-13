377c35c31acc449587f504cf747d233ff59980a0
SanghyeonLee <hyeon012366@gmail.com>
2025-06-13 21:28:15 +0900

    feat: 헤더 테마 토글 적용 및 구조 수정

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,19 +1,39 @@
 'use client';
 
-import Logo from '../common/Logo/Logo';
-import ThemeDropdown from '../common/Dropdown/ThemeDropdown';
+import React from 'react';
+import Button from '@/components/common/Button/Button';
+import ThemeToggle from '../common/Toggle/ThemeToggle';
 import SectionNav from '@/components/ui/SectionNav';
+import { IoIosMenu } from 'react-icons/io';
+import DropdownList from '@/components/common/Dropdown/DropdownList';
+import useSectionNavToggle from '@/hooks/useSectionNavToggle';
+import ALink from '@/components/common/Link/ALink';
 
 export default function Header() {
+  const { toggleDropdown, isDropdownOpen, dropdownRef } = useSectionNavToggle();
+
   return (
-    <header className="bg-transparent backdrop-blur-md fixed top-0 left-0 right-0 w-full z-50 h-18 duration-100 ease-in">
-      <div className="flex-center-between layout-wrap-max-w mx-auto p-content">
-        <Logo />
-        <div className="flex-center-between">
-          <SectionNav />
-          <ThemeDropdown />
+    <header className="header-bg fixed top-0 left-0 right-0 w-full z-50 h-18 duration-100 ease-in">
+      <div className="flex-center-between p-content layout-wrap-max-w mx-auto h-full">
+        <div className="flex-center gap-2">
+          <ALink
+            href={`#intro`}
+            className="hover:opacity-80 text-xl font-extrabold font-cafe"
+            text="상현 포트폴리오"
+          />
+          <ThemeToggle />
         </div>
+        <SectionNav className="hidden sm:flex gap-5 px-2" />
+        <Button
+          onClick={toggleDropdown}
+          className="sm:hidden rounded"
+          aria-expanded={isDropdownOpen}
+          aria-controls="dropdown-list"
+        >
+          <IoIosMenu className="w-8 h-8" />
+        </Button>
       </div>
+      {isDropdownOpen && <DropdownList ref={dropdownRef} className="header-bg" />}
     </header>
   );
 }
