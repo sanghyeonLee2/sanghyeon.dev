5f044082ee1cba67b324001e1a812a0de4ec2510
SanghyeonLee <hyeon012366@gmail.com>
2025-06-13 21:21:49 +0900

    refactor: 인토르 페이지 id 추가 및 로고 컴포넌트 -> ALink 컴포넌트 대체

--- src/components/section/Intro.tsx
+++ src/components/section/Intro.tsx
@@ -1,28 +1,20 @@
 'use client';
 
-import ScrollLink from '@/components/common/Link/ScrollLink';
+import ALink from '@/components/common/Link/ALink';
 import React from 'react';
 const INTRO_TEXT = 'LEE SANG HYEON PORTFOLIO';
 const INTRODUCE = '안녕하세요. 프론트엔드 개발자를 꿈꾸는 이상현 입니다.';
 
 const Intro = () => {
   return (
-    <section className="min-h-screen flex-center flex-col">
-      <div className="flex-center flex-grow">
-        {/* 증명사진 */}
-        {/* <Image
-          alt="증명사진"
-          src="@/assets/IDpicture.jpg"
-          className="w-45 h-45 bg-amber-200 rounded-lg"
-        /> */}
-
-        {/* 소개 텍스트 */}
+    <section id="intro" className="min-h-screen flex-center flex-col">
+      <div className="flex-center flex-1">
         <div className="text-center">
           <h2>{INTRO_TEXT}</h2>
           <p>{INTRODUCE}</p>
         </div>
       </div>
-      <ScrollLink text="더 알아보기" href={'about'} className="w-50 my-5" />
+      <ALink text="더 알아보기" href={'#about'} className="my-5 font-extrabold font-cafe" />
     </section>
   );
 };
