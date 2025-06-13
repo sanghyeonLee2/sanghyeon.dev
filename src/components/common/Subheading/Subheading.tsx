00958e52b61f0af7cc4e8c04e484b7b0ede0b5f2
SanghyeonLee <hyeon012366@gmail.com>
2025-06-13 21:18:56 +0900

    design: 스타일 수정

--- src/components/common/Subheading/Subheading.tsx
+++ src/components/common/Subheading/Subheading.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Subheading = ({ text }: { text: string }) => {
-  return <h3 className="text-2xl font-extrabold pb-5">{text}</h3>;
+  return <p className="text-2xl font-extrabold pb-5">{text}</p>;
 };
 
 export default Subheading;
