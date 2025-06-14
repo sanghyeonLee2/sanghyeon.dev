c4740ea915ea210dd3f50a806bdb935549c1d283
SanghyeonLee <hyeon012366@gmail.com>
2025-06-15 00:02:00 +0900

    feat: 순차 애니메이션 적용

--- src/components/section/Experience/ExperienceList.tsx
+++ src/components/section/Experience/ExperienceList.tsx
@@ -1,28 +1,33 @@
+import { MotionContainer, MotionItem } from '@/components/common/motion';
+import Subheading from '@/components/common/Subheading/Subheading';
 import TextList from '@/components/common/TextList/TextList';
 import { ExperienceType } from '@/components/section/Experience/experience.type';
 import React from 'react';
 
-const ExperienceList = ({ experiences }: { experiences: ExperienceType[] }) => {
+const ExperienceList = ({ contents, subheading }: ExperienceType) => {
   return (
-    <ul className="divide-y divide-gray-300 mb-10">
-      {experiences.map((experience) => (
-        <li key={experience.name}>
-          <article className="flex flex-col md:flex-row gap-6 p-9 my-3">
-            <div className="flex-center">
-              <div className="flex-col md:items-start md:w-70 flex-shrink-0">
-                <h3 className="text-2xl font-semibold content-center text-center">
-                  {experience.name}
-                </h3>
-                <p className="text-sm text-gray-400 text-center">{experience.period}</p>
+    <MotionContainer as="div" amount={0.25}>
+      <Subheading text={subheading} />
+      <ul className="divide-y divide-gray-300 mb-10">
+        {contents.map((content) => (
+          <li key={content.name}>
+            <MotionItem as="article" className="flex flex-col md:flex-row gap-6 p-9">
+              <div className="flex-center">
+                <div className="flex-col md:items-start md:w-70 flex-shrink-0">
+                  <h3 className="text-2xl font-semibold content-center text-center">
+                    {content.name}
+                  </h3>
+                  <p className="text-sm text-gray-400 text-center">{content.period}</p>
+                </div>
               </div>
-            </div>
-            <div className="content-center">
-              <TextList textList={experience.contents} />
-            </div>
-          </article>
-        </li>
-      ))}
-    </ul>
+              <div className="content-center">
+                <TextList textList={content.summaries} />
+              </div>
+            </MotionItem>
+          </li>
+        ))}
+      </ul>
+    </MotionContainer>
   );
 };
 
