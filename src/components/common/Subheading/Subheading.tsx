649879098d92479da744c4eebbf87a55fd9303cd
SanghyeonLee <hyeon012366@gmail.com>
2025-06-14 23:58:15 +0900

    feat: 애니메이션 적용

--- src/components/common/Subheading/Subheading.tsx
+++ src/components/common/Subheading/Subheading.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { MotionItem } from '@/components/common/motion';
 
 const Subheading = ({ text }: { text: string }) => {
-  return <p className="text-2xl font-extrabold pb-5">{text}</p>;
+  return (
+    <MotionItem as="p" className="text-2xl pb-5">
+      {text}
+    </MotionItem>
+  );
 };
 
 export default Subheading;
