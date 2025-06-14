616b27ec522f64296be3b29b13f82cc138ee4087
SanghyeonLee <hyeon012366@gmail.com>
2025-06-15 00:02:42 +0900

    feat: 순차 애니메이션 적용

--- src/components/section/Projects/ImageSlider.tsx
+++ src/components/section/Projects/ImageSlider.tsx
@@ -4,33 +4,36 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import ImageComponent from '@/components/common/Image/ImageComponent';
 import { StaticImageData } from 'next/image';
+import { MotionItem } from '@/components/common/motion';
 
 const ImageSlider = ({ imgs }: { imgs: StaticImageData[] }) => {
   return (
-    <Swiper
-      modules={[Navigation]}
-      navigation
-      spaceBetween={20}
-      breakpoints={{
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-      }}
-      className="w-full h-70"
-    >
-      {imgs.map((img) => (
-        <SwiperSlide key={img.src}>
-          <ImageComponent
-            src={img}
-            alt={`슬라이드 ${img.src}`}
-            className="shadow-[var(--shadow)] rounded-lg h-full"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <MotionItem as="div">
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        spaceBetween={20}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+        }}
+        className="w-full h-70"
+      >
+        {imgs.map((img) => (
+          <SwiperSlide key={img.src}>
+            <ImageComponent
+              src={img}
+              alt={`슬라이드 ${img.src}`}
+              className="shadow-[var(--shadow)] rounded-lg h-full"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </MotionItem>
   );
 };
 
