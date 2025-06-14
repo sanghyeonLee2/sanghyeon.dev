17ac017ba445bc16413bc29a798373375ba62c3c
SanghyeonLee <hyeon012366@gmail.com>
2025-06-15 00:00:11 +0900

    feat: h2 태그 별도 애니메이션 적용

--- src/components/section/Layout.tsx
+++ src/components/section/Layout.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { SectionId } from '@/constants/section';
+import { motion } from 'framer-motion';
 
 interface LayoutProps {
   children: React.ReactNode;
   sectionId: SectionId;
 }
+const headingMotion = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const Layout = ({ children, sectionId }: LayoutProps) => {
   return (
-    <section id={sectionId} className="py-20">
-      <h2 className="capitalize">{sectionId}</h2>
+    <section id={sectionId} className={`${sectionId !== 'intro' && 'py-20'}`}>
+      {sectionId !== 'intro' && (
+        <motion.h2
+          variants={headingMotion}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.5 }}
+          className="capitalize"
+        >
+          {sectionId}
+        </motion.h2>
+      )}
       {children}
     </section>
   );
