bf01efdbcad90d70d5f6af6659657fd1a3427753
SanghyeonLee <hyeon012366@gmail.com>
2025-06-15 00:04:29 +0900

    refactor: 파일 이름 수정 및 불필요 코드 제거

---
+++ src/components/section/Skills/Skills.tsx
@@ -0,0 +1,14 @@
+import CoreSkills from '@/components/section/Skills/CoreSkills';
+import UsedSkills from '@/components/section/Skills/UsedSkills';
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <>
+      <CoreSkills />
+      <UsedSkills />
+    </>
+  );
+};
+
+export default Skills;
