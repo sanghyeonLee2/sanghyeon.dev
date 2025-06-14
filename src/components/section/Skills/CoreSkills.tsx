1275bcf2ac57856f3bb3dbd5c6fda775b6078ea9
SanghyeonLee <hyeon012366@gmail.com>
2025-06-15 00:04:07 +0900

    refactor: 변경된 상수 구조 적용 및 순차 애니메이션 적용

--- src/components/section/Skills/CoreSkills.tsx
+++ src/components/section/Skills/CoreSkills.tsx
@@ -1,20 +1,31 @@
+import { MotionContainer, MotionItem } from '@/components/common/motion';
 import TextList from '@/components/common/TextList/TextList';
-import { CORE_SKILLS } from '@/components/section/Skills/constants';
+import { CORE_SKILL } from './constants';
 import React from 'react';
+import Subheading from '@/components/common/Subheading/Subheading';
 
 const CoreSkills = () => {
   return (
-    <ul className="flex flex-col gap-5 mb-10">
-      {CORE_SKILLS.map(({ icon: Icon, name, description, color }) => (
-        <li key={name} className="p-5 rounded-xl shadow-md bg-[var(--color-sub-bg)]">
-          <div className="flex items-center gap-3 mb-3">
-            <Icon className="w-12 h-12" style={{ color }} />
-            <p className="text-2xl">{name}</p>
-          </div>
-          <TextList textList={description} />
-        </li>
-      ))}
-    </ul>
+    <MotionContainer as="div" amount={0.13}>
+      <Subheading text={CORE_SKILL.subheading} />
+      <ul className="flex flex-col gap-5 mb-10">
+        {CORE_SKILL.skills.map(({ icon: Icon, name, descriptions, color }) => (
+          <MotionItem
+            as="li"
+            key={name}
+            className="p-5 rounded-xl shadow-md bg-[var(--color-sub-bg)]"
+          >
+            <article>
+              <div className="flex items-center gap-3 mb-3">
+                <Icon className="w-12 h-12" style={{ color }} />
+                <p className="text-2xl">{name}</p>
+              </div>
+              <TextList textList={descriptions as string[]} />
+            </article>
+          </MotionItem>
+        ))}
+      </ul>
+    </MotionContainer>
   );
 };
 
--- src/components/section/Skills/UsedSkills.tsx
+++ src/components/section/Skills/UsedSkills.tsx
@@ -1,19 +1,25 @@
-import { USED_SKILLS } from '@/components/section/Skills/constants';
+import { MotionContainer, MotionItem } from '@/components/common/motion';
+import Subheading from '@/components/common/Subheading/Subheading';
+import { USED_SKILL } from '@/components/section/Skills/constants';
 import React from 'react';
 
 const UsedSkills = () => {
   return (
-    <ul className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 gap-4">
-      {USED_SKILLS.map(({ icon: Icon, name, color }) => (
-        <li
-          key={name}
-          className="flex-center flex-col p-content rounded-xl bg-[var(--color-btn-bg)] shadow-[var(--shadow)] transition duration-200"
-        >
-          <Icon className="w-10 h-10 mb-2" style={{ color }} />
-          <span className="text-sm">{name}</span>
-        </li>
-      ))}
-    </ul>
+    <MotionContainer as="div" amount={0.2}>
+      <Subheading text={USED_SKILL.subheading} />
+      <ul className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 gap-4">
+        {USED_SKILL.skills.map(({ icon: Icon, name, color }) => (
+          <MotionItem
+            as="li"
+            key={name}
+            className="flex-center flex-col p-content rounded-xl bg-[var(--color-btn-bg)] shadow-[var(--shadow)]"
+          >
+            <Icon className="w-10 h-10 mb-2" style={{ color }} />
+            <span className="text-sm">{name}</span>
+          </MotionItem>
+        ))}
+      </ul>
+    </MotionContainer>
   );
 };
 
