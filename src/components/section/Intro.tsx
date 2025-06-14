060323c444bd2aeab33bc8650959c313ceaec7ee
SanghyeonLee <hyeon012366@gmail.com>
2025-06-14 23:59:43 +0900

    feat: 추상화 한 MotionContainer, MotionItem순차 애니메이션 적용

--- src/components/section/Intro.tsx
+++ src/components/section/Intro.tsx
@@ -1,21 +1,28 @@
 'use client';
 
-import ALink from '@/components/common/Link/ALink';
 import React from 'react';
+import { MotionContainer, MotionItem } from '@/components/common/motion';
+
 const INTRO_TEXT = 'LEE SANG HYEON PORTFOLIO';
 const INTRODUCE = '안녕하세요. 프론트엔드 개발자를 꿈꾸는 이상현 입니다.';
 
 const Intro = () => {
   return (
-    <section id="intro" className="min-h-screen flex-center flex-col">
+    <MotionContainer as="div" className="min-h-screen flex-center flex-col">
       <div className="flex-center flex-1">
         <div className="text-center">
-          <h2>{INTRO_TEXT}</h2>
-          <p>{INTRODUCE}</p>
+          <MotionItem as="h2" className="text-4xl font-extrabold tracking-[0.05em] mb-3">
+            {INTRO_TEXT}
+          </MotionItem>
+          <MotionItem as="p" className="text-lg font-semibold">
+            {INTRODUCE}
+          </MotionItem>
         </div>
       </div>
-      <ALink text="더 알아보기" href={'#about'} className="my-5 font-extrabold font-cafe" />
-    </section>
+      <MotionItem as="a" href={'#about'} className="my-6 text-[1.1rem] font-extrabold font-cafe">
+        더 알아보기
+      </MotionItem>
+    </MotionContainer>
   );
 };
 
