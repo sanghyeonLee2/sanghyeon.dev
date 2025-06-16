b8b9c2f1de441fb774322c1f307a5bd94ce6a2ab
SanghyeonLee <hyeon012366@gmail.com>
2025-06-16 20:24:16 +0900

    fix: 불필요한 forwardRef 제거해 빌트 오류 픽스

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,27 +1,20 @@
 'use client';
 
 import clsx from 'clsx';
-import React, { forwardRef } from 'react';
+import React from 'react';
 import type { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, text, className, type = 'button', ...rest }, ref) => {
-    return (
-      <button
-        ref={ref}
-        type={type}
-        className={clsx('button-hover transition', className)}
-        {...rest}
-      >
-        {children}
-        {text}
-      </button>
-    );
-  },
-);
+const Button = ({ children, text, className, type = 'button', ...rest }: ButtonProps) => {
+  return (
+    <button type={type} className={clsx('button-hover transition', className)} {...rest}>
+      {children}
+      {text}
+    </button>
+  );
+};
 
 export default Button;
