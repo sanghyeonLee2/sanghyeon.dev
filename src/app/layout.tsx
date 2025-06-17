bcbba357c1ec377ae7a1e421a0e6e3d1d4bd0b80
SanghyeonLee <hyeon012366@gmail.com>
2025-06-18 00:45:16 +0900

    refactor: 불필요 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { CONFIG } from '@/constants/config';
 import '@/styles/globals.css';
 import { pretendard } from '@/fonts/fonts';
 import Header from '@/components/ui/Header';
@@ -10,7 +9,7 @@ export const metadata = defaultMetadata;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={CONFIG.DEFAULT_LANGUAGE} className={pretendard.variable} suppressHydrationWarning>
+    <html lang={'ko'} className={pretendard.variable} suppressHydrationWarning>
       <body className={`${pretendard.className} layout-wrapper`}>
         <ThemeProvider>
           <Header />
