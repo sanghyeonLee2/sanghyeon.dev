708f269f869a45807d2a6066dfa3164452010727
SanghyeonLee <hyeon012366@gmail.com>
2025-06-18 00:51:35 +0900

    refactor: 푸터 구조 수정

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,14 +1,21 @@
 'use client';
 
+import ALink from '@/components/common/Link/ALink';
+import { BLANK_LINK } from '@/constants/dom';
 import { SITE_METADATA } from '@/constants/metaData';
+import { PROFILE_OPTIONS } from '@/constants/section';
 
 export default function Footer() {
   return (
     <footer className="layout-wrap-max-w flex items-center justify-between w-full h-18 p-content">
-      <small className="text-[10px] sm:text-xs text-subtle">{SITE_METADATA.COPY_RIGHT}</small>
-      <a href={SITE_METADATA.MY_GITHUB_URL} className="hover-opacity flex items-center">
-        <small className="text-[10px] sm:text-xs text-subtle">{SITE_METADATA.LINK_TO_GITHUB}</small>
-      </a>
+      <span className="text-[0.75rem] sm:text-sm">{SITE_METADATA.COPY_RIGHT}</span>
+      <ul className="sm:flex gap-3 hidden">
+        {PROFILE_OPTIONS.map(({ href, icon }) => (
+          <li key={href}>
+            <ALink href={href} icon={icon} {...BLANK_LINK} />
+          </li>
+        ))}
+      </ul>
     </footer>
   );
 }
