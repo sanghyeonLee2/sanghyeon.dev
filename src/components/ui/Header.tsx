d43304a86f232ae365fe6c0a013c689d0ba02b78
SanghyeonLee <hyeon012366@gmail.com>
2025-06-17 20:56:37 +0900

    feat: 헤더 스크롤 위치에 따른 스타일 변경 하는 커스텀 훅 사용

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -8,12 +8,19 @@ import { IoIosMenu } from 'react-icons/io';
 import DropdownList from '@/components/common/Dropdown/DropdownList';
 import useSectionNavToggle from '@/hooks/useSectionNavToggle';
 import ALink from '@/components/common/Link/ALink';
+import useScrolled from '@/hooks/useScrolled';
+import clsx from 'clsx';
 
 export default function Header() {
   const { toggleDropdown, isDropdownOpen, dropdownRef } = useSectionNavToggle();
-
+  const scrolled = useScrolled();
   return (
-    <header className="header-bg fixed top-0 left-0 right-0 w-full z-50 h-18 duration-100 ease-in">
+    <header
+      className={clsx(
+        'fixed top-0 left-0 right-0 w-full z-50 h-18 duration-200 ease-in',
+        scrolled ? 'header-bg' : 'bg-transparent',
+      )}
+    >
       <div className="flex-center-between p-content layout-wrap-max-w mx-auto h-full">
         <div className="flex-center gap-2">
           <ALink
