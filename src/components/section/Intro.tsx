a17d5376b0f62a1de04170f1e5b9b64b41a5a8c7
SanghyeonLee <hyeon012366@gmail.com>
2025-06-17 20:52:16 +0900

    feat: 스크롤 하기 전 헤더 투명, 스크롤 시 헤더 배경 색 변경 로직 추가

--- src/components/section/Intro.tsx
+++ src/components/section/Intro.tsx
@@ -3,34 +3,40 @@
 import React from 'react';
 import { MotionContainer, MotionItem } from '@/components/common/motion';
 import introImg from '@/assets/introImg.jpg';
+import { useNextTheme } from '@/hooks/useNextTheme';
 const INTRO_TEXT = 'LEE SANG HYEON PORTFOLIO';
 const INTRODUCE = '안녕하세요. 프론트엔드 개발자를 꿈꾸는 이상현 입니다.';
+import { FaRegCircleDown } from 'react-icons/fa6';
+import ALink from '@/components/common/Link/ALink';
 
 const Intro = () => {
-  // dark: linear-gradient(180deg, rgba(248, 249, 250, 0.95) 0%, rgba(248, 249, 250, 0.8) 100%)
-  // light:linear-gradient(180deg, rgba(26, 31, 40, 0.9) 0%, rgba(26, 31, 40, 0.7)
+  const { currentTheme } = useNextTheme();
   return (
     <MotionContainer
+      key={currentTheme}
       style={{
         backgroundImage: `var(--intro-bg),url(${introImg.src})`,
       }}
       as="div"
-      className="transition-opacity duration-700 opacity-100 relative left-1/2 right-1/2 w-screen -translate-x-1/2 bg-cover bg-center min-h-[50vh] flex-center flex-col"
+      className="p-content animate-fade-in relative left-1/2 right-1/2 w-screen -translate-x-1/2 bg-cover bg-center min-h-[55vh] flex-center"
     >
-      <div className="absolute bottom-0 left-0 right-0 h-50 bg-gradient-to-b from-transparent to-[var(--color-bg)] pointer-events-none z-10" />
-      <div className="flex-center flex-1">
+      <div className="absolute -bottom-1 left-0 bg-gradient-to-b from-transparent to-[var(--color-bg)] right-0 h-50 pointer-events-none z-10" />
+      <div className="flex-center">
         <div className="text-center">
-          <MotionItem as="h2" className="text-4xl font-extrabold tracking-[0.05em] mb-3">
+          <MotionItem as="h1" className="mb-3">
             {INTRO_TEXT}
           </MotionItem>
-          <MotionItem as="p" className="text-lg font-semibold">
+          <MotionItem as="p" className="text-black break-keep dark:text-[var(--color-text)]">
             {INTRODUCE}
           </MotionItem>
         </div>
       </div>
-      {/* <MotionItem as="a" href={'#about'} className="my-6 text-[1.1rem] font-extrabold font-cafe">
-        더 알아보기
-      </MotionItem> */}
+      <ALink
+        icon={
+          <FaRegCircleDown className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce text-[1.6rem] z-20" />
+        }
+        href="#about"
+      />
     </MotionContainer>
   );
 };
