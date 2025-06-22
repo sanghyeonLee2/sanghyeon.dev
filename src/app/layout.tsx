48ac41b039264a0e4a68823e5dbfa890ae090160
SanghyeonLee <hyeon012366@gmail.com>
2025-06-22 23:00:58 +0900

    feat: 기본 레이아웃에 모달 마운트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/ui/Header';
 import { ThemeProvider } from '@/components/providers/ThemeProvider';
 import Footer from '@/components/ui/Footer';
 import { defaultMetadata } from '@/constants/metaData';
+import ImageModal from '@/components/common/Modal/ImageModal';
 
 export const metadata = defaultMetadata;
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProvider>
           <Header />
           {children}
+          <ImageModal />
           <Footer />
         </ThemeProvider>
       </body>
