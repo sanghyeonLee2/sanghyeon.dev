4ccbb2f743156eae2f20f4a56cc9a7bb859f670a
SanghyeonLee <hyeon012366@gmail.com>
2025-07-21 00:33:55 +0900

    refactor: 코드 정리 및 MotionContainer의 key 수정

--- src/components/section/Projects/Projects.tsx
+++ src/components/section/Projects/Projects.tsx
@@ -4,24 +4,27 @@ import { PROJECT } from '@/components/section/Projects/constants';
 import TechStack from './TechStack';
 import LinkOptions from '@/components/section/Projects/LinkOptions';
 import ImageSlider from '@/components/section/Projects/ImageSlider';
-import Subheading from '@/components/common/Subheading/Subheading';
-import TextList from '@/components/common/TextList/TextList';
+import Subheading from '@/components/common/Text/Subheading';
+import TextList from '@/components/common/Text/TextList';
 import { MotionContainer, MotionItem } from '@/components/common/Motion';
 
 const Projects = () => {
   return (
     <ul className="divide-y divide-gray-300">
       {PROJECT.contents.map((content) => (
-        <MotionContainer as="li" amount={0.2} key={content.key} className="h-full py-10">
+        <MotionContainer as="li" amount={0.2} key={content.title} className="h-full py-12">
           <article>
             <MotionItem as="div">
-              <h3>{content.title}</h3>
+              <h3 className="pb-2">{content.title}</h3>
               <p className="text-gray-400 text-sm md:text-[1.1rem]">{content.personnel}</p>
               <p className="text-gray-400 text-sm md:text-[1.1rem]">{content.period}</p>
             </MotionItem>
             <TechStack techStack={content.techStack} />
             <ImageSlider imgs={content.imgs} />
-            <MotionItem as="p" className="p-5 my-5 bg-[var(--color-sub-bg)] rounded-lg">
+            <MotionItem
+              as="p"
+              className="p-5 my-6 bg-[var(--color-sub-bg)] break-keep rounded-lg whitespace-pre-line font-medium"
+            >
               {content.description}
             </MotionItem>
             <div className="mt-8">
