05902b8baeaa1c59a05258a94a836c19a9cfac18
SanghyeonLee <hyeon012366@gmail.com>
2025-07-21 00:35:00 +0900

    feat: 이미지 모듈 추가

---
+++ src/imageModules/projectImages.ts
@@ -0,0 +1,101 @@
+// recipickImages
+import recipick0 from '@/assets/recipick-0.png';
+import recipick1 from '@/assets/recipick-1.png';
+import recipick2 from '@/assets/recipick-2.png';
+import recipick3 from '@/assets/recipick-3.png';
+import recipick4 from '@/assets/recipick-4.png';
+import recipick5 from '@/assets/recipick-5.png';
+import recipick6 from '@/assets/recipick-6.png';
+import recipick7 from '@/assets/recipick-7.png';
+
+export const recipickImages = [
+  recipick0,
+  recipick1,
+  recipick2,
+  recipick3,
+  recipick4,
+  recipick5,
+  recipick6,
+  recipick7,
+];
+
+// godOfDeliveryImages
+import god1 from '@/assets/god-of-delivery-1.png';
+import godArch from '@/assets/architecture.png';
+import god2 from '@/assets/god-of-delivery-2.png';
+import god3 from '@/assets/god-of-delivery-3.png';
+import god4 from '@/assets/god-of-delivery-4.png';
+import god5 from '@/assets/god-of-delivery-5.png';
+import god6 from '@/assets/god-of-delivery-6.png';
+import god7 from '@/assets/god-of-delivery-7.png';
+import god8 from '@/assets/god-of-delivery-8.png';
+import god9 from '@/assets/god-of-delivery-9.png';
+import god10 from '@/assets/god-of-delivery-10.png';
+import god11 from '@/assets/god-of-delivery-11.png';
+
+export const godOfDeliveryImages = [
+  god1,
+  godArch,
+  god2,
+  god3,
+  god4,
+  god5,
+  god6,
+  god7,
+  god8,
+  god9,
+  god10,
+  god11,
+];
+
+// blogReviewCheckerImages
+import blog1 from '@/assets/blog-review-checker-1.png';
+import blog2 from '@/assets/blog-review-checker-2.png';
+import blog3 from '@/assets/blog-review-checker-3.png';
+import blog4 from '@/assets/blog-review-checker-4.png';
+import blog5 from '@/assets/blog-review-checker-5.png';
+
+export const blogReviewCheckerImages = [blog1, blog2, blog3, blog4, blog5];
+
+// coffeeSalesImages
+import cs1 from '@/assets/coffee-sales-1.png';
+import cs2 from '@/assets/coffee-sales-2.png';
+import cs3 from '@/assets/coffee-sales-3.png';
+import cs4 from '@/assets/coffee-sales-4.png';
+import cs5 from '@/assets/coffee-sales-5.png';
+import cs6 from '@/assets/coffee-sales-6.png';
+import cs7 from '@/assets/coffee-sales-7.png';
+import cs8 from '@/assets/coffee-sales-8.png';
+import cs9 from '@/assets/coffee-sales-9.png';
+import cs10 from '@/assets/coffee-sales-10.png';
+import cs11 from '@/assets/coffee-sales-11.png';
+import cs12 from '@/assets/coffee-sales-12.png';
+import cs13 from '@/assets/coffee-sales-13.png';
+import cs14 from '@/assets/coffee-sales-14.png';
+import cs15 from '@/assets/coffee-sales-15.png';
+import cs16 from '@/assets/coffee-sales-16.png';
+
+export const coffeeSalesImages = [
+  cs1,
+  cs2,
+  cs3,
+  cs4,
+  cs5,
+  cs6,
+  cs7,
+  cs8,
+  cs9,
+  cs10,
+  cs11,
+  cs12,
+  cs13,
+  cs14,
+  cs15,
+  cs16,
+];
+
+// portfolioImages
+import portfolio1 from '@/assets/portfolio-1.png';
+import portfolio2 from '@/assets/portfolio-2.png';
+
+export const portfolioImages = [portfolio1, portfolio2];
