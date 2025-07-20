9f2ec59916d2c89aae758d974e6d7618ce97fb33
SanghyeonLee <hyeon012366@gmail.com>
2025-07-21 00:33:50 +0900

    refactor: Subheading 경로 수정 및 MotionItem 그리드 컬럼 수 조정

--- src/components/section/Projects/LinkOptions.tsx
+++ src/components/section/Projects/LinkOptions.tsx
@@ -1,7 +1,7 @@
 import ALink from '@/components/common/Link/ALink';
 import React from 'react';
 import { BLANK_LINK } from '@/constants/dom';
-import Subheading from '@/components/common/Subheading/Subheading';
+import Subheading from '@/components/common/Text/Subheading';
 import { LinkOptionsType } from '@/components/section/Projects/projects.type';
 import { MotionItem } from '@/components/common/Motion';
 
@@ -9,14 +9,14 @@ const LinkOptions = ({ linksOption }: { linksOption: LinkOptionsType }) => {
   return (
     <div className="mt-8">
       <Subheading text={linksOption.subheading} />
-      <MotionItem as="ul" className="grid grid-cols-2 gap-3.5">
+      <MotionItem as="ul" className="grid grid-cols-3 gap-2.5">
         {linksOption.contents.map((option) => (
           <li
             key={option.href}
-            className="shadow-[var(--shadow)] rounded border-[0.05rem] border-gray-300 dark:border-transparent bg-[var(--color-btn-bg)] hover:bg-[var(--color-primary)]/70 transition-colors duration-300"
+            className="shadow-[var(--shadow)] w-full rounded border-[0.05rem] border-gray-300 dark:border-transparent bg-[var(--color-btn-bg)] hover:bg-[var(--color-primary)]/70 transition-colors duration-300"
           >
             <ALink
-              className="flex gap-3 flex-center font-semibold text-[1rem] sm:text-[1.1rem] text-inherit py-3 px-3"
+              className="gap-2.5 font-semibold text-sm sm:text-base flex-center text-inherit py-3 px-1"
               {...option}
               {...BLANK_LINK}
               onClick={(e) => e.stopPropagation()}
